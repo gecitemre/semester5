fb8a2ac3f8e2b9bfd9688f462811ac11f97e4b6c
Emre Geçit <emregecit2@gmail.com>
2022-10-29 00:05:15 +0300

    the1

--- CENG315/THE1/the1.cpp
+++ CENG315/THE1/the1.cpp
@@ -6,35 +6,126 @@ Otherwise, split the array into K sub-arrays and do K recursive calls to sort th
 Then, merge K sorted arrays.
 When merging K sorted-arrays, you should use a Binary Min Heap to select the minimum element between the minimum elements of K partition arrays.
 When creating the array of the heap,
-Firstly, generate a linear array whose elements are the minimum elements of the K partition arrays. At the beginning, the position of the each element is determined by the beinting partition. For example, the element coming from partition 0 is placed to heap_array[0] and the element coming from partition 1 is placed to heap_array[1] etc.
+Firstly, generate a linear array whose elements are the minimum elements of the K partition arrays. At the beginning, the position of the each element is determined by the belonging partition. For example, the element coming from partition 0 is placed to heap_array[0] and the element coming from partition 1 is placed to heap_array[1] etc.
 Then, heapify the initial array.   
 After finding the minimum element, you should insert a new element from the related partition to the Min Heap.
 Read the minimum element in the heap and record it.
 Then, replace the minimum element with a new element from the partition that has the last minimum element.(New element insertion is not a swap operation. Swap has to be counted only inside the heap or insertion sort.)
 Then, heapify the current array.*/
 
-void heapify(int *heap_array, int heap_size, int i)
+void insertion_sort(int* arr, long &comparison, long & swap, int size) 
 {
-    int smallest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    if (left < heap_size && heap_array[left] < heap_array[smallest])
-        smallest = left;
-    if (right < heap_size && heap_array[right] < heap_array[smallest])
-        smallest = right;
-    if (smallest != i)
+    int i, j, key;
+    for (i = 1; i < size; i++)
     {
-        swap(heap_array[i], heap_array[smallest]);
-        heapify(heap_array, heap_size, smallest);
+        key = arr[i];
+        j = i-1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j];
+            j = j-1;
+            swap++;
+            comparison++;
+        }
+        arr[j+1] = key;
+        comparison++;
     }
 }
 
-int main() {
-    int K = 4;
-    int size = 12;
-    int arr[12] = {12, 11, 13, 5, 6, 7, 1, 2, 3, 4, 8, 9};
-    heap h(arr, size);
-    for (int i = 0; i < size; i++) {
-        cout << h.array[i] << " ";
+void heapify(int* arr, int size, int i, long &comparison, long &swap)
+{
+    int smallest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+    if (left < size && arr[left] < arr[smallest])
+    {
+        smallest = left;
     }
+    if (right < size && arr[right] < arr[smallest])
+    {
+        smallest = right;
+    }
+    comparison+=2;
+    if (smallest != i)
+    {
+        swap++;
+        int temp = arr[i];
+        arr[i] = arr[smallest];
+        arr[smallest] = temp;
+        heapify(arr, size, smallest, comparison, swap);
+    }
+}
+
+void build_heap(int* arr, int size, long &comparison, long &swap)
+{
+    for (int i = size/2 - 1; i >= 0; i--)
+    {
+        heapify(arr, size, i, comparison, swap);
+    }
+}
+
+int kWayMergeSortWithHeap(int* arr, int K, int size, long& comparison, long& swap){
+    int number_of_calls = 1;
+    if (size < K) {
+        insertion_sort(arr, comparison, swap, size);
+        return number_of_calls;
+    }
+    int copy[size];
+    for (int i = 0; i < size; i++)
+    {
+        copy[i] = arr[i];
+    }
+    for (int i = 0; i < K; i++) {
+        number_of_calls += kWayMergeSortWithHeap(copy + i * (size / K), K, size / K, comparison, swap);
+    }
+    int* heap = new int[K], *indices = new int[K];
+    for (int i = 0; i < K; i++) {
+        heap[i] = copy[i * (size / K)];
+        indices[i] = 1;
+    }
+    build_heap(heap, K, comparison, swap);
+    int sorted_length = 0;
+
+    while (true) {
+        int min2 = INT_MAX, min2_index = -1;
+        for (int i = 0; i < K; i++) {
+            if (indices[i] < size / K && copy[i * (size / K) + indices[i]] <= min2) {
+                min2 = copy[i * (size / K) + indices[i]];
+                min2_index = i;
+            }
+        }
+        if (min2_index == -1) {
+            break;
+        }
+        int min = heap[0];
+        arr[sorted_length++] = min;
+        heap[0] = min2;
+        indices[min2_index]++;
+        heapify(heap, K, 0, comparison, swap);
+    }
+    while (sorted_length < size) {
+        arr[sorted_length++] = heap[0];
+        heap[0] = INT_MAX;
+        heapify(heap, K, 0, comparison, swap);
+    }
+    delete[] heap;
+    delete[] indices;
+    return number_of_calls;
+}
+
+
+
+int main()
+{
+    int size, K;
+    size = 12;
+    K = 3;
+    int arr[size] = { 15, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+    long comparison = 0, swap = 0;
+    kWayMergeSortWithHeap(arr, K, size, comparison, swap);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
