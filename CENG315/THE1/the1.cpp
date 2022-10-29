554622271823c29796bfb078bf18988c41401118
emregecit2 <emregecit5@gmail.com>
2022-10-29 23:11:34 +0300

    Grade: 100/100

--- CENG315/THE1/the1.cpp
+++ CENG315/THE1/the1.cpp
@@ -20,15 +20,13 @@ void insertion_sort(int *arr, long &comparison, long &swap, int size)
     {
         key = arr[i];
         j = i - 1;
-        while (j >= 0 && arr[j] > key)
+        while (j >= 0 && (comparison++, arr[j] > key))
         {
             arr[j + 1] = arr[j];
             j = j - 1;
             swap++;
-            comparison++;
         }
         arr[j + 1] = key;
-        comparison++;
     }
 }
 
