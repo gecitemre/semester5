46f8d24fedc615120ea278664cdf7c76bc5017ea
emregecit2 <emregecit5@gmail.com>
2022-11-04 15:08:52 +0300

    100/100

--- CENG315/THE2/the2.cpp
+++ CENG315/THE2/the2.cpp
@@ -9,8 +9,14 @@ long multiDigitRadixSort(int *arr, bool ascending, int arraySize, int groupSize,
     int count[base];
     int temp[arraySize];
 
-    for (int start = 1; start < pow(10, maxDigitLength); start *= base)
+    for (int h = 0; h < maxDigitLength; h+=groupSize)
     {
+        if (h + groupSize > maxDigitLength)
+        {
+            groupSize = maxDigitLength - h;
+            base = (int)pow(10, groupSize);
+        }
+        int start = pow(10, h);
         for (int i = 0; i < base; i++)
         {
             count[i] = 0;
@@ -41,6 +47,6 @@ long multiDigitRadixSort(int *arr, bool ascending, int arraySize, int groupSize,
             arr[i] = temp[i];
             numberOfIterations++;
         }
-    }
+   }
     return numberOfIterations;
 }
