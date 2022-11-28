763d66ca94a6c9e9681a8ff589caa54032cde0e7
gecitemre <emregecit5@gmail.com>
2022-11-29 00:33:48 +0300

    Renamed raw files, deleted test scripts

--- CENG331/AttackLab/target65/test.sh
+++
@@ -1,6 +0,0 @@
-#bin/bash
-for i in 1 2 3; do
-    ./hex2raw -i phase$i/ctarget.l$i | ./ctarget -i
-done
-./hex2raw -i phase4/rtarget.l2 | ./ctarget -i
-tar -czvf e2521581.tar.gz phase1/ctarget.l1 phase2/ctarget.l2 phase3/ctarget.l3 phase4/rtarget.l2
