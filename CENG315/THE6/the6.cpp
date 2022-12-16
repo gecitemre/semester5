67561e6b0f67b521730590131ada2f9c9166c72f
gecitemre <emregecit5@gmail.com>
2022-12-17 01:58:37 +0300

    77/100

--- CENG315/THE6/the6.cpp
+++ CENG315/THE6/the6.cpp
@@ -78,7 +78,6 @@ void quick_sort(vector<Pipeline *> &pipelines, int left, int right)
 
 vector<int> plan_min_cost_pipeline_usage(vector<Pipeline *> pipelines, int num_of_buildings)
 {
-
     vector<int> solution;
     for (int i = 0; i < num_of_buildings; i++)
     {
@@ -129,49 +128,55 @@ vector<int> plan_min_cost_pipeline_usage(vector<Pipeline *> pipelines, int num_o
     return solution; // this is a dummy return value. YOU SHOULD CHANGE THIS!
 }
 
-vector<Pipeline *> dfs(vector<vector<Pipeline*>> &graph, bool *visited, int current, int aim)
+Pipeline* dfs(vector<vector<Pipeline *>> &graph, bool *visited, int current, int aim)
 {
     visited[current] = true;
     for (Pipeline *p : graph[current])
     {
+        if (p->end1 == p->end2)
+            continue;
         int neighbor = p->end1 + p->end2 - current;
         if (neighbor == aim)
         {
-            vector<Pipeline *> result;
-            result.push_back(p);
-            return result;
+            return p;
         }
         if (!visited[neighbor])
         {
-            vector<Pipeline *> result = dfs(graph, visited, neighbor, aim);
-            if (result.size() > 0)
+            Pipeline* result = dfs(graph, visited, neighbor, aim);
+            if (result == NULL)
             {
-                result.push_back(p);
-                return result;
+                return p;
+            }
+            else {
+                if (result->cost_of_consumption > p->cost_of_consumption)
+                    return result;
+                else
+                    return p;
             }
         }
     }
-    return vector<Pipeline *>();
+    return NULL;
 }
-
-bool visited[0b1 << 18];
 
 vector<int> optimize_plan(vector<Pipeline *> current_solution, Pipeline *new_pipeline)
 {
-    vector<vector<Pipeline*>> graph(current_solution.size());
-    for (Pipeline *p : current_solution)
+    bool visited[current_solution.size()];
+    vector<vector<Pipeline *>> graph(current_solution.size());
+    for (int i = 0; i < current_solution.size(); i++)
     {
-        graph[p->end1].push_back(p);
-        graph[p->end2].push_back(p);
+        visited[i] = false;
+        graph[current_solution[i]->end1].push_back(current_solution[i]);
+        graph[current_solution[i]->end2].push_back(current_solution[i]);
     }
 
-    vector<Pipeline *> path = dfs(graph, visited, new_pipeline->end1, new_pipeline->end2);
-    if (path.size() == 0)
+    Pipeline* max_edge = dfs(graph, visited, new_pipeline->end1, new_pipeline->end2);
+    if (max_edge == NULL)
     {
-        int maximum = furnace_cost_f(new_pipeline->end1) > furnace_cost_f(new_pipeline->end2) ? new_pipeline->end1 : new_pipeline->end2;
+        int maximum = furnace_cost_f(new_pipeline->end1) > furnace_cost_f(new_pipeline->end2) ? ancestor(new_pipeline->end1) : ancestor(new_pipeline->end2);
 
         if (new_pipeline->cost_of_consumption < furnace_cost_f(maximum))
         {
+            int max_cost = 0, max_index = -1;
             for (int i = 0; i < current_solution.size(); i++)
             {
                 if (current_solution[i]->end1 == maximum && current_solution[i]->end2 == maximum)
@@ -181,24 +186,24 @@ vector<int> optimize_plan(vector<Pipeline *> current_solution, Pipeline *new_pip
                 }
             }
         }
-        
     }
     else
     {
-        int max_cost = 0, max_index = -1;
-        for (int i = 0; i < path.size(); i++)
+        int max_index = -1;
+        for (int i = 0; i < current_solution.size(); i++)
         {
-            if (path[i]->cost_of_consumption > max_cost)
+            if (current_solution[i] == max_edge)
             {
-                max_cost = path[i]->cost_of_consumption;
                 max_index = i;
+                break;
             }
         }
-        if (max_cost > new_pipeline->cost_of_consumption)
+        if (max_edge->cost_of_consumption > new_pipeline->cost_of_consumption)
         {
             current_solution[max_index] = new_pipeline;
         }
     }
+
     vector<int> solution;
     for (Pipeline *p : current_solution)
     {
