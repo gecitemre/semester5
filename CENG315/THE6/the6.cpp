3e6a6524e81990fc76ea975b5780b0a73f78db88
gecitemre <emregecit5@gmail.com>
2022-12-16 21:54:37 +0300

    70/100

--- CENG315/THE6/the6.cpp
+++ CENG315/THE6/the6.cpp
@@ -13,20 +13,32 @@ using namespace std;
 */
 
 int parent[0b1 << 18];
-pair<int, int> furnace_cost[0b1 << 18];
+int furnace_cost[0b1 << 18];
 
 int ancestor(int p) {
     if (parent[p] == p) return p;
     return parent[p] = ancestor(parent[p]);
 }
 
+
+int furnace_cost_f(int p) {
+    if (ancestor(p) == p) return furnace_cost[p];
+    return furnace_cost_f(parent[p]);
+}
+
 void merge(int p, int q) {
-    parent[ancestor(p)] = ancestor(q);
+    if (furnace_cost_f(p) < furnace_cost_f(q)) {
+        parent[ancestor(q)] = ancestor(p);
+    }
+    else {
+        parent[ancestor(p)] = ancestor(q);
+    }
 }
 
 bool connected(int p, int q) {
     return ancestor(p) == ancestor(q);
 }
+
 
 void quick_sort(vector<Pipeline*> &pipelines, int left, int right) {
     int i = left, j = right;
@@ -58,19 +70,30 @@ void quick_sort(vector<Pipeline*> &pipelines, int left, int right) {
 vector<int> plan_min_cost_pipeline_usage(vector<Pipeline*> pipelines, int num_of_buildings) {
     
     vector<int> solution;
-    for (int i = 0; i < num_of_buildings; i++) parent[i] = i;
+    for (int i = 0; i < num_of_buildings; i++){
+        parent[i] = i;
+        furnace_cost[i] = INT_MAX;
+    }
 
     quick_sort(pipelines, 0, pipelines.size() - 1);
     cout << "sorted: ";
     for (int i = 0; i < pipelines.size(); i++) {
-        cout << pipelines[i]->id << " ";
         if (pipelines[i]->end1 == pipelines[i]->end2) {
-            furnace_cost[pipelines[i]->end1] = {pipelines[i]->cost_of_consumption, pipelines[i]->id};
+            if (pipelines[i]->cost_of_consumption < furnace_cost_f(pipelines[i]->end1)) {
+                furnace_cost[parent[pipelines[i]->end1]] = pipelines[i]->cost_of_consumption;
+                solution.push_back(pipelines[i]->id);
+                cout << "connected: " << pipelines[i]->id << " " << pipelines[i]->end1 << " " << pipelines[i]->end2 << endl;
+            }
         }
         else if (!connected(pipelines[i]->end1, pipelines[i]->end2)) {
-            merge(pipelines[i]->end1, pipelines[i]->end2);
-            solution.push_back(pipelines[i]->id);
-            cout << "connected: " << pipelines[i]->end1 << " " << pipelines[i]->end2 << endl;
+            if (furnace_cost_f(pipelines[i]->end1) == INT_MAX || furnace_cost_f(pipelines[i]->end2) == INT_MAX) {
+                merge(pipelines[i]->end1, pipelines[i]->end2);
+                solution.push_back(pipelines[i]->id);
+                cout << "connected: " << pipelines[i]->id << " " << pipelines[i]->end1 << " " << pipelines[i]->end2 << endl;
+            }
+        }
+        else {
+            cout << "skipped: " << pipelines[i]->id << " " << pipelines[i]->end1 << " " << pipelines[i]->end2 << endl;
         }
     }
     cout << endl;
@@ -93,15 +116,13 @@ vector<int> plan_min_cost_pipeline_usage(vector<Pipeline*> pipelines, int num_of
         cout << i << " ";
     }
     cout << endl;
-    vector<pair<int, int>> component_min(num_of_buildings, {INT_MAX, -1});
+    vector<int> component_min(num_of_buildings, INT_MAX);
     for (int i = 0; i < num_of_buildings; i++) {
-        if (furnace_cost[i].first < component_min[ancestor(i)].first) {
+        if (furnace_cost[i] < component_min[ancestor(i)]) {
             component_min[ancestor(i)] = furnace_cost[i];
         }
     }
-    for (int i: components) {
-        solution.push_back(component_min[i].second);
-    }
+
     return solution; // this is a dummy return value. YOU SHOULD CHANGE THIS!
     
 }
