03f8a6b472538662c694876aa253e38348c19c64
gecitemre <emregecit5@gmail.com>
2022-12-28 00:07:52 +0300

    save

--- CENG351/PA2/student_pack/PlaylistTree.java
+++ CENG351/PA2/student_pack/PlaylistTree.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
 public class PlaylistTree {
-	
-	public PlaylistNode primaryRoot;		//root of the primary B+ tree
-	public PlaylistNode secondaryRoot;	//root of the secondary B+ tree
+
+	public PlaylistNode primaryRoot; // root of the primary B+ tree
+	public PlaylistNode secondaryRoot; // root of the secondary B+ tree
+
 	public PlaylistTree(Integer order) {
 		PlaylistNode.order = order;
 		primaryRoot = new PlaylistNodePrimaryLeaf(null);
@@ -11,18 +12,16 @@ public class PlaylistTree {
 		secondaryRoot = new PlaylistNodeSecondaryLeaf(null);
 		secondaryRoot.level = 0;
 	}
-	
+
 	public void addSong(CengSong song) {
 		if (primaryRoot instanceof PlaylistNodePrimaryLeaf) {
 			PlaylistNodePrimaryLeaf primaryRootAsLeaf = (PlaylistNodePrimaryLeaf) primaryRoot;
 			if (primaryRootAsLeaf.songCount() == 2 * PlaylistNode.order) {
 				// split
 
-			}
-			else {
+			} else {
 				ArrayList<CengSong> songs = primaryRootAsLeaf.getSongs();
 				// insert song into songs using bisect right
-
 				int low = 0;
 				int high = songs.size();
 				int mid = 0;
@@ -30,66 +29,93 @@ public class PlaylistTree {
 					mid = (low + high) / 2;
 					if (song.audioId() > songs.get(mid).audioId()) {
 						low = mid + 1;
-					}
-					else {
+					} else {
 						high = mid;
 					}
 				}
 				primaryRootAsLeaf.addSong(low, song);
 			}
-		}
-		else {
+		} else {
 			PlaylistNodePrimaryIndex primaryRootAsInternal = (PlaylistNodePrimaryIndex) primaryRoot;
-			primaryRootAsInternal.addSong(song);
+			
 		}
+
+		if (secondaryRoot instanceof PlaylistNodeSecondaryLeaf) {
+			PlaylistNodeSecondaryLeaf secondaryRootAsLeaf = (PlaylistNodeSecondaryLeaf) secondaryRoot;
+			if (secondaryRootAsLeaf.genreCount() == 2 * PlaylistNode.order) {
+				// split
+
+			} else {
+				ArrayList<ArrayList<CengSong>> songBucket = secondaryRootAsLeaf.getSongBucket();
+				// insert song into songs using bisect right
+
+				int low = 0;
+				int high = songBucket.size();
+				int mid = 0;
+				while (low < high) {
+					mid = (low + high) / 2;
+					if (song.genre().compareTo(songBucket.get(mid).get(0).genre()) > 0) {
+						low = mid + 1;
+					} else {
+						high = mid;
+					}
+				}
+			}
+		} else {
+			PlaylistNodeSecondaryIndex secondaryRootAsInternal = (PlaylistNodeSecondaryIndex) secondaryRoot;
+			
+		}
+
 	}
-	
+
 	public CengSong searchSong(Integer audioId) {
 		if (primaryRoot instanceof PlaylistNodePrimaryLeaf) {
 			PlaylistNodePrimaryLeaf primaryRootAsLeaf = (PlaylistNodePrimaryLeaf) primaryRoot;
-			return primaryRootAsLeaf.searchSong(audioId);
-		}
-		else {
+			// TODO
+			return null;
+		} else {
 			PlaylistNodePrimaryIndex primaryRootAsInternal = (PlaylistNodePrimaryIndex) primaryRoot;
 			return primaryRootAsInternal.searchSong(audioId);
 		}
 	}
-	
-	
+
 	public void printPrimaryPlaylist() {
 		// TODO: Implement this method
 		// print the primary B+ tree in Depth-first order
-		/*All non-leaf and leaf nodes should be printed with a proper indentation (with tabs) for each
-		level in the tree.
-		For non-leaf nodes, you should print the search key enclosed by <index> and </index> tags.
-		For leaf nodes, you should print the content between <data> and </data> tags. Records
-		should be printed between with <record> and </record> tags. */
+		/*
+		 * All non-leaf and leaf nodes should be printed with a proper indentation (with
+		 * tabs) for each
+		 * level in the tree.
+		 * For non-leaf nodes, you should print the search key enclosed by <index> and
+		 * </index> tags.
+		 * For leaf nodes, you should print the content between <data> and </data> tags.
+		 * Records
+		 * should be printed between with <record> and </record> tags.
+		 */
 
 		if (primaryRoot instanceof PlaylistNodePrimaryLeaf) {
 			PlaylistNodePrimaryLeaf primaryRootAsLeaf = (PlaylistNodePrimaryLeaf) primaryRoot;
-			primaryRootAsLeaf.print(0);
-		}
-		else {
+			// TODO
+		} else {
 			PlaylistNodePrimaryIndex primaryRootAsInternal = (PlaylistNodePrimaryIndex) primaryRoot;
 			primaryRootAsInternal.print(0);
 		}
 	}
-	
+
 	public void printSecondaryPlaylist() {
 		// TODO: Implement this method
 		// print the secondary B+ tree in Depth-first order
 
 		if (secondaryRoot instanceof PlaylistNodeSecondaryLeaf) {
 			PlaylistNodeSecondaryLeaf secondaryRootAsLeaf = (PlaylistNodeSecondaryLeaf) secondaryRoot;
-			secondaryRootAsLeaf.print(0);
-		}
-		else {
+			// TODO
+		} else {
 			PlaylistNodeSecondaryIndex secondaryRootAsInternal = (PlaylistNodeSecondaryIndex) secondaryRoot;
 			secondaryRootAsInternal.print(0);
 		}
 		return;
 	}
-	
+
 	// Extra functions if needed
 
 }
